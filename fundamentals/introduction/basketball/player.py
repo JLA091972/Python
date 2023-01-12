e005490039709bc92974b9aaf7371fb781bb7df6
Juleus Louis (Jay) Alquizalas <juleuslouis@gmail.com>
2023-01-11 22:57:24 -0700

    Basketball solution

---
+++ fundamentals/introduction/basketball/player.py
@@ -0,0 +1,15 @@
+from players import players
+
+class Player:
+    def __init__(self, data):
+        self.name = data['name']
+        self. age = data['age']
+        self.position = data['position']
+        self.team = data['team']
+
+player_objects = []
+
+for player in players:
+    player_objects.append(Player(player))
+    
+
