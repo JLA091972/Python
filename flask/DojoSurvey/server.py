41c38a5abd8e88b9e338d6c14d948b4455272b85
Juleus Louis (Jay) Alquizalas <juleuslouis@gmail.com>
2023-01-14 21:24:47 -0800

    Solution for Dojo Survey

---
+++ flask/DojoSurvey/server.py
@@ -0,0 +1,23 @@
+
+from flask import Flask, render_template, request, redirect, session
+
+
+app = Flask(__name__)
+app.secret_key = "sapientia et doctrina:wisdom and learning"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods = ['post'])
+def result():
+    print(request.form)
+    session['name'] = request.form['name']
+    session['dojoloc'] = request.form['dojoloc']
+    session['favlang'] = request.form['favlang']
+    session['comments'] = request.form ['comments']
+    return render_template('displayresult.html')
+
+if __name__=="__main__":
+    app.run(debug=True)    # Run the app in debug mode.  
+
