4b161f3f4fc76d3a97b071bbf0cd4cb749fc19db
Juleus Louis (Jay) Alquizalas <juleuslouis@gmail.com>
2023-01-21 21:51:41 -0700

    updating

--- flask_mysql/crud/users_crud_module/flask_app/__init__.py
+++ flask_mysql/crud/users_crud_module/flask_app/__init__.py
@@ -1,5 +1,5 @@
 # import flask from Flask
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
 # app.secret_key = "sapientia et doctrina:wisdom and learning"
